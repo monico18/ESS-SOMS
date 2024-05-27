bc24b64d063b9b67a48a75c9124d294294f4b7ec
Sneuc <pedro.safarista@gmail.com>
2024-05-27 15:56:56 +0100

    Live Feed

--- vue/SOMS/src/components/user/Dashboard.vue
+++ vue/SOMS/src/components/user/Dashboard.vue
@@ -1,6 +1,56 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
+const apiKey = 'c321147f32bb6d31e61b897e4d724a3e';
+const city = ref('Leiria');
+
+const weatherDescription = ref('');
+const temperature = ref('');
+const humidity = ref('');
+
+let intervalId = null;
+
+const fetchWeather = async () => {
+  const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  const params = new URLSearchParams({
+    q: city.value,
+    appid: apiKey,
+    units: 'metric'
+  });
+
+  try {
+    const response = await fetch(`${baseUrl}?${params.toString()}`);
+    if (response.ok) {
+      const weatherData = await response.json();
+      weatherDescription.value = weatherData.weather[0].description;
+      temperature.value = weatherData.main.temp;
+      humidity.value = weatherData.main.humidity;
+    } else {
+      console.error(`Error: Failed to retrieve weather data (HTTP ${response.status})`);
+    }
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+};
+
+const startFetching = () => {
+  if (!intervalId) {
+    fetchWeather();
+    intervalId = setInterval(fetchWeather, 30000); 
+  }
+};
+
+const stopFetching = () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+};
+
+onMounted(startFetching);
+onUnmounted(stopFetching);
+
+const videoUrl = ref('http://10.0.0.5:5000/video_feed');
 
 </script>   
 <template>
@@ -9,7 +59,19 @@ import { ref } from 'vue';
     <div class="d-flex justify-content-center">
       <h1>Welcome User</h1>
     </div>
+    <div class="weather-info">
+      <h3>Weather in {{ city }}:</h3>
+      <h6>Description: {{ weatherDescription }}</h6>
+      <h6>Temperature: {{ temperature }} °C</h6>
+      <h6>Humidity: {{ humidity }}%</h6>
+    </div>
     <br>
+    <div class="d-flex justify-content-center">
+      <h3>Live Feed</h3>
+    </div>
+    <div class="d-flex justify-content-center">
+      <img :src="videoUrl" alt="Live Stream" class="live-stream">
+    </div>
 
 </template>
 <style>
@@ -31,5 +93,13 @@ import { ref } from 'vue';
 .algorithms{
     display: flexbox;
     flex-direction: row;
+}
+.weather-info {
+  text-align: center;
+  margin-top: 20px;
+}
+.live-stream {
+  width: 640px;
+  height: 480px;
 }
 </style>
